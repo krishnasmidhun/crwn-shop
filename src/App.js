3d86d78b2c24504ad0f6b6f30a4152ae5b630ddf
krishnasmidhun <krishnasmidhun@gmail.com>
2020-08-31 16:43:08 +0530

    Directory component and menu item Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from "./pages/home-page.component";
+
 function App() {
   return (
-    <div className="App">
-
+    <div>
+        <HomePage/>
     </div>
   );
 }
---
+++ src/pages/home-page.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './home-page.styles.scss';
+import Directory from "../components/directory/directory.component";
+const HomePage=()=>(
+    <div className="home-page">
+        <Directory/>
+
+    </div>
+);
+export default HomePage;
